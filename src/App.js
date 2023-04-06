69d9e4e333f2179e2afb59acf5b033037768ddbf
Avani <107024504+Anu467@users.noreply.github.com>
2023-04-06 10:45:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter,Route,Switch} from 'react-router-dom';
+import Home from './pages';
+import SigninPage from './pages/signin';
+import SignupPage from './pages/signup';
+import ContactUsPage from './pages/contact';
+import MainComponent from './components/MainComponent/MainComponent.js'
+import Profile from './pages/Profile';
+
+function App() {
+  return (
+      <BrowserRouter basename= "/React-Web-Application">
+        <Switch>
+          <Route path='/' component={Home} exact />
+          <Route path='/signup' component={SignupPage} exact />
+          <Route path='/signin' component={SigninPage} exact />
+          <Route path='/profile' component={() => <Profile authorized={false} />} exact />
+          <Route path='/contact' component={ContactUsPage} exact />
+          <Route path="/analyse" exact element={<MainComponent />} />
+        </Switch>
+      </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/InfoSection2/Data2.js
@@ -0,0 +1,5 @@
+export const homeObjFour = {
+  id: 'predict',
+  lightBg: true,
+  
+};
